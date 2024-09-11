5f03960cf327f08c1a2a72fc47fdf78911fe9444
John Wohlbier <jgwohlbier@sei.cmu.edu>
2024-09-11 16:51:32 -0400

    add loop on acc invocation

--- sw/baremetal/espacc.c
+++ sw/baremetal/espacc.c
@@ -208,7 +208,8 @@ int main(int argc, char * argv[])
 
 #ifndef __riscv
 		for (coherence = ACC_COH_NONE; coherence <= ACC_COH_RECALL; coherence++) {
-#else
+#else           
+                for (size_t ii=0; ii<2; ii++)
 		{
 			/* TODO: Restore full test once ESP caches are integrated */
 			coherence = ACC_COH_NONE;
